6c8a64818c2f34d2cb9839dbf1f4c7a0f0f596c9
asu.ma <maa_2514-kadai@yahoo.co.jp>
2021-12-07 14:38:25 +0000

    kadai2

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,19 +1,20 @@
 def fizzbuzz(num)
 
   if num % 15 == 0
-    puts "fizzbuzz"
+    return "FizzBuzz"
   elsif  num % 3 == 0
-    puts "fizz"
+    return "Fizz"
   elsif num % 5 == 0
-    puts "buzz"
+    return "Buzz"
   else
-    puts num
+    return num
   end
 end
+
 
    
 
 num_max = 100
 (1..num_max).each do |num|
-  fizzbuzz(num)
+  puts fizzbuzz(num)
 end
